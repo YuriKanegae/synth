89f19bd2813d0cc4a88c657d57288b5c6b08365a
ykanegae <yurikanegae@gmail.com>
2025-06-30 22:14:38 -0300

    change project to react app

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import './index.css';
+
+import React    from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App      from './App';
+
+const element = document.getElementById('root') as HTMLElement;
+const root    = ReactDOM.createRoot( element );
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
